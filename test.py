d80f683ff4d16a520053738b08cb7d3d6bc49898
cipher421 <chmodgaurav@gmail.com>
2026-09-15 12:35:58 +0530

    Fix

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from langchain_openrouter import ChatOpenRouter
 
-llm = ChatOpenRouter(model = "google/gemma-4-31b-it:free",temperature=0)
+llm = ChatOpenRouter(model = "google/gemma-4-31b-it-20260402:free",temperature=0)
 print(llm.model_name)
